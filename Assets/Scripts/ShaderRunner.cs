76f61f95244eae96ad6ec7d847a8ae453798ffa6
Mohammad Hassan Sadeghein <h.sadeghein96@gmail.com>
2019-10-14 15:54:50 +0300

    N-Body Simulation

---
+++ Assets/Scripts/ShaderRunner.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderRunner : MonoBehaviour
+{
+    public int particleCount = 1000;
+    public Material material;
+    public ComputeShader computeShader;
+
+    private const int SIZE_OF_PARTICLE = 24;
+    private int computeShaderKernelID;
+    private const int NUMBER_OF_PARTICLE_IN_EACH_THREAD = 256;
+    private ComputeBuffer computeBuffer;
+    private int threadNumber;
+
+    private AudioSource audioData;
+    private struct Particle
+    {
+        public Vector3 position;
+        public Vector3 velocity;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        Inititalize();
+
+    }
+    void OnDestroy()
+    {
+        if (computeBuffer != null)
+            computeBuffer.Release();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Inititalize();
+        }
+        computeShader.SetFloat("_deltaTime", Time.deltaTime);
+
+        computeShader.Dispatch(computeShaderKernelID, threadNumber, 1, 1);
+    }
+
+    private void OnRenderObject()
+    {
+        material.SetPass(0);
+        Graphics.DrawProceduralNow(MeshTopology.Points, 1, particleCount);
+
+    }
+    
+    private void Inititalize()
+    {
+        RenderSettings.ambientLight = Color.black;
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+        if (particleCount <= 0)
+            particleCount = 1;
+        threadNumber = Mathf.CeilToInt((float)particleCount / NUMBER_OF_PARTICLE_IN_EACH_THREAD);
+
+        Particle[] particles = new Particle[particleCount];
+        for (int i = 0; i < particleCount; i++)
+        {
+            if (i % 4 == 0)
+            {
+                particles[i].position.x = Random.value * 20 - 4.0f;
+                particles[i].position.y = Random.value * 20 - 4.0f;
+                particles[i].position.z = Random.value * 10 - 4.0f;
+                particles[i].velocity.x = 0;
+                particles[i].velocity.y = 0;
+                particles[i].velocity.z = 0;
+
+            }
+            else if (i % 4 == 1)
+            {
+                particles[i].position.x = Random.value * 45 + 100.0f;
+                particles[i].position.y = Random.value * 4 + 100.0f;
+                particles[i].position.z = Random.value * 10 + 10.0f;
+                particles[i].velocity.x = 0;
+                particles[i].velocity.y = 0;
+                particles[i].velocity.z = 0;
+
+            }
+            else if (i % 4 == 2)
+            {
+                particles[i].position.x = Random.value * 40 - 250.0f;
+                particles[i].position.y = Random.value * 40 - 4.0f;
+                particles[i].position.z = Random.value * 10 - 4.0f;
+                particles[i].velocity.x = 0;
+                particles[i].velocity.y = 0;
+                particles[i].velocity.z = 0;
+            }
+            else
+            {
+                particles[i].position.x = Random.value * 40 + -200.0f;
+                particles[i].position.y = Random.value * 40 + 250.0f;
+                particles[i].position.z = Random.value * 10 + 10.0f;
+                particles[i].velocity.x = 0;
+                particles[i].velocity.y = 0;
+                particles[i].velocity.z = 0;
+            }
+
+
+        }
+
+        computeBuffer = new ComputeBuffer(particleCount, SIZE_OF_PARTICLE);
+        computeBuffer.SetData(particles);
+
+        computeShaderKernelID = computeShader.FindKernel("CSMain");
+
+        computeShader.SetBuffer(computeShaderKernelID, "particleBuffer", computeBuffer);
+        material.SetBuffer("particleBuffer", computeBuffer);
+        computeShader.SetInt("_numofParticles", particleCount);
+    }
+
+}
---
+++ Assets/Shaders/LitShader.shader
@@ -0,0 +1,60 @@
+﻿Shader "Universe/LitShader"
+{
+	Properties
+	{
+		[HDR]_ColorLow("Color Slow Speed", Color) = (0, 0, 0.5, 0.3)
+		[HDR]_ColorHigh("Color High Speed", Color) = (1, 0, 0, 0.3)
+		[HDR]_ColorPos("Color Position", Color) = (1, 0, 0, 0.3)
+
+		_HighSpeedValue("High speed Value", Range(0, 500)) = 25
+	}
+		SubShader
+	{
+		Tags { "RenderType" = "Transparent" "Queue"="Transparent" }
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct v2f
+			{
+				float4 position : SV_POSITION;
+				float4 color : COLOR;
+			};
+
+			struct Particle {
+				float3 position;
+				float3 velocity;
+			};
+
+			StructuredBuffer<Particle> particleBuffer;
+
+			float4 _ColorLow, _ColorHigh, _ColorPos;
+			float _HighSpeedValue;
+
+			v2f vert(uint vertex_id : SV_VertexID, uint instance_id : SV_InstanceID)
+			{
+				v2f o;
+				o.position = UnityObjectToClipPos(float4(particleBuffer[instance_id].position, 1.0f));
+
+				float speed = length(particleBuffer[instance_id].velocity);
+				float lerpValue = clamp(speed / _HighSpeedValue, 0.0f, 1.0f);
+				o.color.rgb = lerp(_ColorLow.rgb, _ColorHigh.rgb, lerpValue);
+				o.color.rgb = lerp(o.color.rgb, _ColorPos.rgb, clamp(length(particleBuffer[instance_id].position) / 1000.0f,0.0f,1.0f));
+				o.color.a = (_ColorLow.a * _ColorHigh.a);
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				return i.color;
+			}
+			ENDCG
+		}
+	}
+}
